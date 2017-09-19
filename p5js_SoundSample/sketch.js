2e1106c98b765177fcdb458df0a45045a7efa247
lkokwei1998 <32089588+lkokwei1998@users.noreply.github.com>
2017-09-19 16:13:12 +0800

    Update sketch.js

--- p5js_SoundSample/sketch.js
+++ p5js_SoundSample/sketch.js
@@ -18,7 +18,7 @@ function setup() {
 function draw() {
     // Draw a background that fades to black
     noStroke();
-    fill(128, 0, 0, 10);
+    fill(67, 153, 245, 10);
     rect(0, 0, width, height);
     // The getLevel() method returns values between 0 and 1,
     // so map() is used to convert the values to larger numbers
